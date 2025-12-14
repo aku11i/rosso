5a4a2e2149c4a3ea6c07e8817901f9e1e6f8db74
Shuhei Akutagawa <shuhei.akutagawa+github@gmail.com>
2025-12-14 18:27:33 +0900

    test: remove index export check

--- src/index.test.ts
+++
@@ -1,7 +0,0 @@
-import assert from 'node:assert/strict';
-import test from 'node:test';
-import { fetchSource } from './index.ts';
-
-test('index exports fetchSource function', () => {
-  assert.equal(typeof fetchSource, 'function');
-});
